973c89d2313d252f23a55ac1823c2490a2b1dce7
LikoKuridze <lkuridze@credo.ge>
2024-10-27 23:19:15 +0400

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+
+    {
+
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} x {i} = {result}");
+        }
+    }
+}
+
+
